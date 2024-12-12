83350fc029c9d81c4c36aa82c29e6b1a82f415a0
Eric Zhang <superezzhang@gmail.com>
2024-12-11 22:47:49 -0500

    Implement database functionality

--- inMemoryDataBase.py
+++ inMemoryDataBase.py
@@ -1,27 +1,55 @@
-class InMemoryDB:
+class inMemoryDataBase:
     def __init__(self):
         # Initialize database storage
-        pass
+        self.dataBase = {}
+        self.transationDataBase = None
+        self.transationInProgress = False
     
     def begin_transaction(self):
         # Starts a new transaction 
-        pass
+        if self.transationInProgress:
+            raise RuntimeError("A Transaction is already in progress")
+        
+        # Initalize a transaction, set up transaction history 
+        self.transationDataBase = dict(self.dataBase)
+        self.transationInProgress = True
     
     def put(self, key: str, val: int):
-        # Store a key-value pair
-        pass
+        # Put key value pair into database
+
+        # Error Check
+        if not self.transationInProgress:
+            raise RuntimeError("A Transaction is not in progress")
+        self.transationDataBase[key] = val
     
     def get(self, key: str) -> int:
         # Get the value associated with the key
-        pass
+        
+        # If transaction is in progress, read from trasaction database
+        # Otherwise, read from main database
+        if self.transaction_in_progress:
+            return self.transationDataBase.get(key, None)
+        else:
+            return self.dataBase.get(key, None)
     
     def commit(self):
         # Commit the current transaction
-        pass
+        if not self.transationInProgress:
+            raise RuntimeError("A Transaction is not in progress")
+        
+        self.dataBase = dict(self.transationDataBase)
+
+        # Reset transaction to a unused state
+        self.transationDataBase = None
+        self.transationInProgress = False
     
     def rollback(self):
         # Rollback the current transaction
-        pass
+        if not self.transationInProgress:
+            raise RuntimeError("A Transaction is not in progress")
+        
+        self.transationDataBase = None
+        self.transationInProgress = False
 
 
 
