66e027b9ad317733d4fa9d4166a71da2e0a74e42
Eric Zhang <superezzhang@gmail.com>
2024-12-11 23:52:00 -0500

    Add rest of unit tests

--- unitTests.py
+++ unitTests.py
@@ -14,6 +14,7 @@ class testInMemoryDataBase(unittest.TestCase):
         # Verify the value was stored correctly
         self.assertEqual(self.dataBase.get("test_key"), 123)
     
+
     def testInstruction2(self):
         self.dataBase.begin_transaction()
 
@@ -42,6 +43,7 @@ class testInMemoryDataBase(unittest.TestCase):
         self.dataBase.commit()
         self.assertEqual(self.dataBase.get("A"), 10)  # Value should be updated
     
+
     def testInstruction4(self):
         self.dataBase.begin_transaction()
 
@@ -53,10 +55,12 @@ class testInMemoryDataBase(unittest.TestCase):
         # Test getting a non existent key 
         self.assertEqual(self.dataBase.get("B"), None)
     
+
     def testInstruction5(self):
         # Checks if put not during an transaction will throw an exception
         with self.assertRaises(RuntimeError):
             self.dataBase.put("A", 10)
+
 
     def testInstruction6(self):
 
@@ -66,15 +70,79 @@ class testInMemoryDataBase(unittest.TestCase):
         # Check functionality of get inside a transaction
         self.dataBase.begin_transaction()
         self.assertEqual(self.dataBase.get("A"), None)
-
-
-
-
-        
-
     
 
+    def testInstruction7(self):
+        # Start a new transaction
+        self.dataBase.begin_transaction()
+    
 
+    def testInstruction8(self):
+        self.dataBase.begin_transaction()
+
+        # Expect error for new transaction when one is already in progress 
+        with self.assertRaises(RuntimeError):
+            self.dataBase.begin_transaction()
+    
+
+    def testInstruction9(self):
+        self.dataBase.begin_transaction()
+        self.dataBase.put("A", 1)
+        self.dataBase.commit()
+
+        self.dataBase.begin_transaction()
+        self.dataBase.put("B", 2)
+        self.dataBase.put("A", 3)
+
+        # Test visibility of transactions
+        # We expect 1 and None as outputs for get as they are not visible yet
+
+        self.assertEqual(self.dataBase.get("A"), 1)
+        self.assertEqual(self.dataBase.get("B"), None)
+
+
+    def testInstruction10(self):
+        # Test transaction ends after commit
+        self.dataBase.begin_transaction()
+        self.dataBase.put("A", 1)
+        self.dataBase.commit()
+        # Verify transaction ended by checking that put raises error
+        with self.assertRaises(RuntimeError):
+            self.dataBase.put("B", 2) 
+
+        # Test transaction ends after rollback
+        self.dataBase.begin_transaction()
+        self.dataBase.put("C", 3)
+        self.dataBase.rollback()
+        # Verify transaction ended by checking that put raises error
+        with self.assertRaises(RuntimeError):
+            self.dataBase.put("D", 4)  
+
+
+    def testInstruction11(self):
+        # Verify key doesn't exist initially
+        self.assertEqual(self.dataBase.get("A"), None)
+        self.dataBase.begin_transaction()
+        self.dataBase.put("A", 1)
+        self.dataBase.commit()
+
+        # Verify future get() can see the committed changes
+        self.assertEqual(self.dataBase.get("A"), 1)
+
+
+    def testInstruction12(self):
+        self.dataBase.begin_transaction()
+        self.dataBase.put("A", 1)
+        self.dataBase.commit()
+
+        # Verify A intially is 1
+        self.assertEqual(self.dataBase.get("A"), 1)
+        self.dataBase.begin_transaction()
+        self.dataBase.put("A", 2)
+        self.dataBase.rollback()
+        
+        # Verify that A is still 1
+        self.assertEqual(self.dataBase.get("A"), 1)
 
 
 if __name__ == "__main__":
