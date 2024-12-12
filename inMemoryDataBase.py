cc54ea069108e996c9a75746f767cbd61d3671dd
Eric Zhang <superezzhang@gmail.com>
2024-12-11 22:27:00 -0500

    Implement framework

---
+++ inMemoryDataBase.py
@@ -0,0 +1,28 @@
+class InMemoryDB:
+    def __init__(self):
+        # Initialize database storage
+        pass
+    
+    def begin_transaction(self):
+        # Starts a new transaction 
+        pass
+    
+    def put(self, key: str, val: int):
+        # Store a key-value pair
+        pass
+    
+    def get(self, key: str) -> int:
+        # Get the value associated with the key
+        pass
+    
+    def commit(self):
+        # Commit the current transaction
+        pass
+    
+    def rollback(self):
+        # Rollback the current transaction
+        pass
+
+
+
+
